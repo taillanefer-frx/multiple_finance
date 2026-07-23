6b27a21a2792a5e0a6b4cee8d6a603900e8da974
taillanefer-frx <taillanefer@gmail.com>
2026-07-23 10:02:43 -0300

    Inicializa e estiliza o modo claro e escuro

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './app/App'
 import { registerServiceWorker } from './app/pwa'
+import { initializeColorMode } from './features/profile/theme'
 import './index.css'
+
+initializeColorMode()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
